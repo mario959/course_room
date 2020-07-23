6ceb70d0b754decf6b772151c4a55e9ca6433716
mario959 <60920627+mario959@users.noreply.github.com>
2020-07-23 13:33:35 +0200

    Marios first commit

---
+++ Classroom/Form1.cs
@@ -0,0 +1,35 @@
+﻿using ControlzEx.Standard;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Classroom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Btnclick_Click(object sender, EventArgs e)
+        {
+            //using ( var facilityEntities = new ())
+            //{
+            //    var facility = new facility();
+            //    facility.Name = "";
+            //    facilityEntities.Facilities.Add(facility);
+            //    facilityEntities.SaveChanges();
+            //}
+        }
+    }
+}
